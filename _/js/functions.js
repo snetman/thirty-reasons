3fb33e6d8b261cc56e83f39a175f86aa758d1ad8
Adam Snetman <snetman@gmail.com>
2012-08-27 17:12:05 -0400

    done w main nav

--- _/js/functions.js
+++ _/js/functions.js
@@ -6,7 +6,19 @@
 $(document).ready(function (){
 
 	// your functions go here
-
+	
+	// Show and hide the list-based nav
+	
+	$('#nav-toggle').find('a.show-nav').bind('click focus', function() {
+  	$('#site-nav').toggleClass('is-hidden');
+  	
+	});
+	/*
+	$('.nav-primary').find('h3')
+   .bind('click focus', function(){
+      $(this).parent().toggleClass('expanded')
+   });
+   */
 });
 
 
